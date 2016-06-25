93106e14f70962b77328333e0e529349e7e86612
Simon Carrignon <simon.carrignon@gmail.com>
2016-06-25 03:49:45 +0200

    some correction

--- extraction.py
+++ extraction.py
@@ -6,7 +6,8 @@ from os.path import isfile, join
 path="trade/processed/"
 years =listdir(path)
 print(years)
-ofile="year,place,flow,flow,boat\n"
+ofile="year,country,boat,volume,type\n"
+
 for year in years:
     print(year)
     filename=path+str(year)+"/"+str(year)+"_1.csv"
@@ -16,15 +17,23 @@ for year in years:
     for row in csvfile:
         #print(row)
         text=row[0]
+        text=text.lower()
         m1=re.search("(.+) vessel",text)
         m2=re.search("steam",text)
         if(m1):
             print("vessel")
             print(m1.group(1))
             cname=m1.group(1)
-            ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+",vessel"+"\n"
-        if(m2):
-            ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+",steamboat"+"\n"
+            btype="vessel"
+            #ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+",vessel"+"\n"
+        elif(m2):
+            btype="steamboat"
+            #ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+",steamboat"+"\n"
+        else:
+            print("nothing")
+            cname=text
+        btype="vessel"
+        ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+","+btype+"\n"
         #if( ):
         #    print(text)
         #    spl=text.split(" ")
