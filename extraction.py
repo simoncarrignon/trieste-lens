186068e8284dd8cc90c907386c097c8cfe85734a
Simon Carrignon <simon.carrignon@gmail.com>
2016-07-02 04:33:42 +0200

    Correction of numerous bugs in extraction.py and handling of empty row

--- extraction.py
+++ extraction.py
@@ -7,14 +7,19 @@ from os.path import isfile, join
 def read_csv(filename):
     f = open(filename, 'r')
     csvfile=csv.reader(f)
+    #header=csvfile.next()
+    cname=""
+    btype=""
     
     ofile=""
     for row in csvfile:
         #print(row)
-        text=row[0]
-        text=text.lower()
-        m1=re.search("(.+) vessel",text)
-        m2=re.search("steam",text)
+        text=row[0].strip()
+        text=text.lower().replace("\x96","-")
+        text=re.sub(r'[\W_]+',' ',text)
+        m1=re.search("^(.+) vessel[s]?",text)
+        m2=re.search("^(.+) steamboat",text)
+        m3=re.search("^steamboat",text)
         if(m1):
             #print("vessel")
             print(m1.group(1))
@@ -22,18 +27,27 @@ def read_csv(filename):
             btype="vessel"
             #ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+",vessel"+"\n"
         elif(m2):
-            btype="steam"
+            print(m2.group(1))
+            cname=m2.group(1)
             btype="steamboat"
             #ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+",steamboat"+"\n"
+        elif(m3):
+            #cname=text
+            btype="steamboat"
         else:
-            print("nothing")
-            cname=text
-            btype="vessel"
-        ofile=ofile+str(year)+","+cname+","+row[1]+","+row[2]+","+btype+"\n"
-        #if( ):
-        #    print(text)
-        #    spl=text.split(" ")
-        #    print(spl[0]+"--"+row[1])
+            if(text!=''):
+                cname=text
+                btype="vessel"
+            else:
+                print("")
+                #print("!!!!!PROBLEME!!!!")
+                #print(row)
+                #print(filename)
+
+        if(cname!=""):
+            mtot=re.search("total",cname)
+            if(not mtot and (row[1] != "" or row[2]!="")):
+                ofile=ofile+str(year)+","+cname.lower()+","+row[1]+","+row[2]+","+btype+"\n"
             
     f.close()
     return(ofile)
