ee77cb480248a3ab1277cd26817016658a7c7e9b
Simon <simon.carrignon@gmail.com>
2017-11-13 16:04:38 +0100

    update graph analysis trieste export importe volumes

--- script/divTrieste.R
+++ script/divTrieste.R
@@ -8,11 +8,11 @@ source("functionTrieste.R")
 #write.csv(
 #
 ##Import data
-allimport=read.csv("final_table_dist-import.csv")
-allimport=read.csv("201609-final_table_dist-import_CLEANED.csv")
-allexport=read.csv("201609-final_table_dist-export_CLEANED.csv")
-allexport=read.csv("final_table_dist-export.csv")
-events=read.csv("events.csv",sep="\t",header=F)
+allimport=read.csv("../data/final_table_dist-import.csv")
+#allimport=read.csv("../data/01609-final_table_dist-import_CLEANED.csv")
+#allexport=read.csv("../data/01609-final_table_dist-export_CLEANED.csv")
+allexport=read.csv("../data/final_table_dist-export.csv")
+events=read.csv("../data/events.csv",sep="\t",header=F)
 
 allimport=na.omit(allimport)
 allexport=na.omit(allexport)
@@ -22,7 +22,7 @@ write.csv(print(unique(c(as.character(allexport$country),as.character(allimport$
 #allExpLong=addCoordinateAndDist(allexport)
 allImpLong=allimport
 allExpLong=allexport
-allimport=allimport[ grep("total",allimport$country,invert=T) ,]
+allimport=allimport[ grep("total",allimport$country,invert=T) ,] #useless as it seems dataset has been cleaned
 
 #Reorder the data by year (better visualisation)
 #allexport=allexport[order(allexport$year),]
@@ -116,3 +116,49 @@ par(mar=c(4,7,1,1))
 image(t(as.matrix(log10(subAllI))),axes=F,col= colorRampPalette(c("blue", "green"))(n = 1000))
 axis(1,at=seq(0,1,length.out=ncol(subAllI)/3),labels=colnames(subAllI)[seq(1,ncol(subAllI),length.out=ncol(subAllI)/3)],las=2)
 axis(2,at=seq(0,1,length.out=nrow(subAllI)),labels=rownames(subAllI)[seq(1,nrow(subAllI),length.out=nrow(subAllI))],las=2)
+
+
+
+plot(unique(allimport$year),tapply(allimport$volume , allimport$year, sd)/tapply(allimport$volume , allimport$year, mean),type="l",col="red")
+points(unique(allexport$year),tapply(allexport$volume , allexport$year, sd)/tapply(allexport$volume , allexport$year, mean),type="l")
+
+plot(unique(allimport$year),apply(tapply(allimport$volume , allimport[c("year", "new_loc")],sd),1,mean,na.rm=TRUE)/apply(tapply(allimport$volume , allimport[c("year", "new_loc")],sum),1,mean,na.rm=TRUE),type="l" ,ylab="")
+points(unique(allexport$year),apply(tapply(allexport$volume , allexport[c("year", "new_loc")],sd),1,mean,na.rm=TRUE)/apply(tapply(allexport$volume , allexport[c("year", "new_loc")],sum),1,mean,na.rm=TRUE),type="l" ,col="red" )
+
+##plot number of country
+plot(unique(allimport$year),tapply(allimport$new_loc,allimport$year,length),type="l",title="nb of new_loc",ylim=c(0,60),ylab="Number of Country",xlab="")
+
+par(mfrow=c(2,2))
+###plot sd only 
+plot(unique(allimport$year),apply(tapply(allimport$volume , allimport[c("year", "new_loc")],sd),1,sd,na.rm=TRUE),main="evol. of sd",log="y",ylab="sd",xlab="")
+###plot means only
+plot(unique(allimport$year),apply(tapply(allimport$volume , allimport[c("year", "new_loc")],mean),1,mean,na.rm=TRUE),main="evol. of mean",log="y",ylab="mean",xlab="")
+
+###plot ration sd/mean
+sumvessels=tapply(allimport$volume , allimport[c("year", "new_loc")],sum)
+sumvesselse=tapply(allexport$volume , allexport[c("year", "new_loc")],sum)
+plot(unique(allimport$year),apply(sumvessels,1,sd,na.rm=TRUE)/(apply(sumvessels,1,mean,na.rm=TRUE)),type="l" ,ylab="sd/mean",xlab="")
+plot(unique(allexport$year),apply(sumvesselse,1,sd,na.rm=TRUE)/(apply(sumvesselse,1,mean,na.rm=TRUE)),type="l",col="red")
+points(unique(allexport$year),apply(tapply(allexport$volume , allexport[c("year", "new_loc")],sd),1,mean,na.rm=TRUE)/apply(tapply(allexport$volume , allexport[c("year", "new_loc")],sum),1,mean,na.rm=TRUE),type="l" ,col="red" )
+
+###Add the name of the big players
+for(y in unique(allimport$year)){
+    alli=allimport[allimport$year == y,]
+    vol=tapply(alli$volum,alli$new_loc,sum)
+text(rep(y,10),seq(.5,1.5,.1),labels=names(sort(vol,decreasing=T))[1:10],cex=.5)
+}
+
+
+meanvol=tapply(allimport$volume , allimport[c("year", "new_loc")],mean)
+totavol=apply(meanvol,1,sum,na.rm=T)
+barplot(log(t(meanvol/totavol)))
+barplot(t(tapply(allimport$volume , allimport[c("year", "new_loc")],mean)/apply(tapply(allimport$volume , allimport[c("year", "new_loc")],mean),2,sum,na.rm=T)))
+
+color=rainbow(length(unique(allimport$new_loc)))
+par(mfrow=c(1,2))
+ barplot(t(wght),space=0,border=NA,col=color)
+numloc=length(unique(allimport$new_loc))
+ plot(rep(1,numloc),1:length(unique(allimport$new_loc)),col=color,pch=20,axes=F,xlab="",ylab="",cex=2)
+ text(rep(1.2,numloc),1:numloc,label=unique(as.character(allimport$new_loc)))
+ image(as.matrix((wght) ) )
+
